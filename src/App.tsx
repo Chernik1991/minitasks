bad86e7f3b07ca35c6c93b94ab463fbc6fa53cd7
Chernik1991 <5059585@mail.ru>
2022-02-14 17:04:51 +0300

    useState Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import { App2 } from './minitask2_button/App2';
+import {App2} from './minitask2_button/App2';
 import {App1} from './site/App1';
+import {App3} from './minitask3_useState/App3';
 
 
 function App() {
     return (<>
         {/*<App1 />*/}
-        <App2 />
+        {/*<App2 />*/}
+            <App3 />
 </>
 
     )
---
+++ src/minitask3_useState/App3.tsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './App3.css';
+
+export function App3() {
+// let a=1
+    let[a, setA]=useState(1)
+    const onClickHandler=()=>{
+        setA(++a)
+        console.log(a)
+    }
+    const onClickHandler0=()=>{
+        setA(a=0)
+        console.log(a)
+    }
+    return (
+<div>
+    <h1>{a}</h1>
+    <button onClick={onClickHandler}>number</button>
+    <button onClick={onClickHandler0}>0</button>
+</div>
+    )
+}
